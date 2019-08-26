3e78c7b1e24d98febe6220d4f690aeee43230e74
Tandi Samples <tandisamples@gmail.com>
2019-08-26 01:38:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
 
 end
+
+string = ""
+  count = 0
+  array.each do |el|
+    string << el
+    count += 1 
+    f array.size == 2 
+      string = array.join(" and ")
+      elsif array.size > 2 && count < array.size
+        count == array.size - 1 ? string << ", and " : string << ", "
+    end
+    
+  end
